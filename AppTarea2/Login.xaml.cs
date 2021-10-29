1af170326d6bc66dc681ef692ea74a0e856bf58a
josua <josua@DESKTOP-AA73CCT>
2021-10-29 16:36:27 -0500

    Agregar archivos de proyecto.

---
+++ AppTarea2/Login.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppTarea2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnIngreso_Clicked(object sender, EventArgs e)
+        {
+            string Usuario = txtUsuario.Text;
+            string clave = txtClave.Text;
+
+            if(Usuario == "estudiante2021" && clave == "uisrael2021")
+            {
+                await Navigation.PushAsync(new MainPage(Usuario));
+            }
+            else
+            {
+                await DisplayAlert("Advertencia", "Clave o Usuario incorrecto", "OK");
+            }
+        }
+    }
+}
---
+++ AppTarea2/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppTarea2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage(string usuario)
+        {
+            InitializeComponent();
+            lblUsuarioC.Text = usuario;
+        }
+
+        private void btnSuma_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double NotaUno = Convert.ToDouble(txtS1.Text);
+                double ExamenUno = Convert.ToDouble(txtE1.Text);
+
+                double Nf1 = NotaUno * 0.3;
+                double Ef1 = ExamenUno * 0.2;
+
+                double suma = Nf1 + Ef1;
+                txtP1.Text = Convert.ToString(suma);
+
+                double NotaDos = Convert.ToDouble(txtS2.Text);
+                double ExamenDos = Convert.ToDouble(txtE2.Text);
+
+                double Nf2 = NotaDos * 0.3;
+                double Ef2 = ExamenDos * 0.2;
+
+                double suma2 = Nf2 + Ef2;
+                txtP2.Text = Convert.ToString(suma2);
+
+                double NotaFinal = suma + suma2;
+                txtNf.Text = Convert.ToString(NotaFinal);
+
+                if(NotaFinal >= 7)
+                {
+                    txtEstado.Text = Convert.ToString("Aprobado");
+                } else
+                {
+                    txtEstado.Text = Convert.ToString("Reprobado");
+                }
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Mensaje de Advertencia", ex.Message, "OK");
+            }
+        }
+    }
+}
